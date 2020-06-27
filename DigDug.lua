b2a4d548643a8e8943502d4a566fed9c0a7418c0
KyrosKrane <kyros@kyros.info>
2020-06-27 17:10:22 -0500

    Initial code setup

---
+++ DigDug.lua
@@ -0,0 +1,21 @@
+-- DigDug.lua
+-- Written by KyrosKrane Sylvanblade (kyros@kyros.info)
+-- Copyright (c) 2020 KyrosKrane Sylvanblade
+-- Licensed under the MIT License, as per the included file.
+-- Addon version: @project-version@
+
+--#########################################
+--# Description
+--#########################################
+
+-- This addon fixes the archaeology progress bar to correctly show nine segments when appropriate.
+
+--#########################################
+--# Description
+--#########################################
+
+-- Grab the WoW-defined addon folder name and storage table for our addon
+local addonName, DigDug = ...
+
+
+print("Hello world from DigDug!")
