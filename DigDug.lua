887d635f125a61127b10cc8c3ca6a524412ade2d
KyrosKrane <kyros@kyros.info>
2020-06-29 00:53:23 -0500

    Initial showing of the new texture

--- DigDug.lua
+++ DigDug.lua
@@ -17,5 +17,31 @@
 -- Grab the WoW-defined addon folder name and storage table for our addon
 local addonName, DigDug = ...
 
+--@end-alpha@
+DigDug.DebugMode = true
+--@end-alpha@
 
-print("Hello world from DigDug!")
+print("Hello world from >>" .. addonName .. "<<!")
+
+local frame = _G["ArcheologyDigsiteProgressBar"]
+local BarBorderAndOverlay = frame.BarBorderAndOverlay
+
+local NineBarPath = "Interface\\Addons\\DigDug\\ArcheologyToast9.blp"
+
+if frame then
+	print("dumping Frame")
+	DigDug.Utilities.DumpTable(frame)
+else
+	print("Frame is not valid")
+end
+
+if BarBorderAndOverlay then
+	print("dumping BarBorderAndOverlay")
+	DigDug.Utilities.DumpTable(BarBorderAndOverlay)
+else
+	print("BarBorderAndOverlay is not valid")
+end
+
+
+BarBorderAndOverlay:SetTexture(NineBarPath)
+BarBorderAndOverlay:SetTexCoord(0.00390625, 0.76953125, 0.08984375, 0.15625000)
